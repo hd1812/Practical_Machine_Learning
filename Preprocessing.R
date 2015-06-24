bc9554d8763d4af06fece5593dd9b0e28bba50f1
hd1812 <hao.ding12@imperial.ac.uk>
2015-06-24 23:20:35 +0100

    initial commit

--- Preprocessing.R
+++ Preprocessing.R
@@ -4,16 +4,40 @@ data(spam)
 inTrain<-createDataPartition(y=spam$type,p=0.75,list=FALSE)
 training<-spam[inTrain,]
 testing<-spam[-inTrain,]
-dim(training)
 
-##generalised linear model fit
-set.seed(32343)
-modelFit<-train(type~.,data=training,method="glm")
-modelFit
+##bug in following code
+hist(training$capatalAve,main="",xlab="ave.capital run length")
 
-##predictions
-predictions<-predict(modelFit,newdata=testing)
-predictions
+##standalising variables in training set
+trainCapAve<-training$capitalAve
+testCapAveS<-(trainCapAve-mean(trainCapAve))/sd(trainCapAve)
 
-##confusionMatrix
-confusionMatrix(predictions,testing$type)
+##standardizing test set. Using mean and std of TRAINING set
+testCapAve<-testing$capitalAve
+testCapAveS<-(testCapAve-mean(trainCapAve))/sd(trainCapAve)
+
+##preprocessing function
+preObj <- preProcess(training[,-58],method=c("center","scale"))
+trainCapAveS <- predict(preObj, training[,-58])$capitalAve
+mean(trainCapAveS)
+
+##boxcox, contineous data
+preObj <- preProcess(training[,-58],method=c("BoxCox"))
+trainCapAveS <- predict(preObj, training[,-58])$capitalAve
+mean(trainCapAveS)
+
+set.seed(13343)
+
+#Make some values NA
+training$capAve<-training$capitalAve
+selectNA<-rbinom(dim(training)[1],size=1,prob=0.05)==1
+training$capAve[selectNA]<-NA
+
+#Impute and standardize, MA filter
+preObj <- preProcess(training[,-58],method=c("BoxCox"))
+capAve<-predict(preObj,training[,-58])$capAve
+
+#standardize true values
+capAveTruth<-training$capitalAve
+capAveTruth<-(capAveTruth-mean(capAveTruth))/sd(capAveTruth)
+
--- TrainingOption.R
+++ TrainingOption.R
@@ -4,4 +4,16 @@ data(spam)
 inTrain<-createDataPartition(y=spam$type,p=0.75,list=FALSE)
 training<-spam[inTrain,]
 testing<-spam[-inTrain,]
+dim(training)
+
+##generalised linear model fit
+set.seed(32343)
 modelFit<-train(type~.,data=training,method="glm")
+modelFit
+
+##predictions
+predictions<-predict(modelFit,newdata=testing)
+predictions
+
+##confusionMatrix
+confusionMatrix(predictions,testing$type)
